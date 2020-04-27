c9b75b4f8ec0ec5c3623c32ea1a9d41c1b1d51c3
Henri HANNETEL <henri.hannetel@gmail.com>
2020-04-27 09:29:17 +0200

    [wedding-list] reduced js functions size

--- templates/components/wedding-list.js
+++ templates/components/wedding-list.js
@@ -1,55 +1,65 @@
-var url = new URL(location.href)
+if ('content' in document.createElement('template')) {
+  var item_template = document.querySelector('#item');
+} else {
+  alert('Votre navigateur est trop ancien pour afficher cette page, veuillez le mettre à jour.')
+}
+var url = new URL(location.href);
+var items_ul = document.querySelector("#items");
+var categories_select = document.querySelector('#categories');
 
 function init_items_cards(items) {
-  let items_ul = document.querySelector("#items");
   let categories = new Set()
-  if ('content' in document.createElement('template')) {
-    var item_template = document.querySelector('#item');
-  } else {
-    alert('Votre navigateur est trop ancien pour afficher cette page, veuillez le mettre à jour.')
-  }
   for (item of items) {
-    let item_li = item_template.content.cloneNode(true);
-    item_li.querySelector(".text-body").textContent = item.name;
-    item_li.querySelector(".text-muted").textContent = `${item.price / 100} €`;
-    items_ul.appendChild(item_li);
-    item_li = items_ul.lastElementChild;
-    item_li.setAttribute('data-category',
-                         item.category.normalize("NFD").replace(/[\u0300-\u036f]/g, ""));
-    item_li.querySelector(".card-img-top")
-      .setAttribute('src', `/img/wedding-list/${item.image}`);
+    _add_item_li(item);
     categories.add(item.category);
   }
-  categories_select = document.getElementById('categories');
   for (category of Array.from(categories).sort()) {
-    let category_option = document.createElement("option");
-    category_option.value = category.normalize("NFD")
-                              .replace(/[\u0300-\u036f]/g, "");
-    category_option.textContent = category;
-    categories_select.appendChild(category_option);
+    _add_category_option(category);
   }
   let url_category = url.searchParams.get('category')
   if (url_category) {
-    document.getElementById('categories').value = url_category;
+    categories_select.value = url_category;
     refresh_items_cards();
   }
 }
 
 function refresh_items_cards() {
   selected_category = document.getElementById('categories').value;
-  if (selected_category == 'Toutes') {
-    url.searchParams.delete('category', selected_category);
-    window.history.pushState("", "", location.pathname);
-  } else {
-    url.searchParams.set('category', selected_category);
-    window.history.pushState("", "", `${location.pathname}?${url.searchParams}`);
-  }
-  for (item_li of document.querySelector("#items").querySelectorAll("li")) {
+  _set_url_category(selected_category);
+  for (item_li of items_ul.querySelectorAll("li")) {
     if (selected_category == 'Toutes' || item_li.getAttribute('data-category') == selected_category) {
       item_li.style.display = "initial";
     } else {
       item_li.style.display = "none";
     }
+  }
+}
+
+function _add_category_option(category) {
+  let category_option = document.createElement("option");
+  category_option.value = category.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  category_option.textContent = category;
+  categories_select.appendChild(category_option);
+}
+
+function _add_item_li(item) {
+  let item_li = item_template.content.cloneNode(true);
+  item_li.querySelector(".text-body").textContent = item.name;
+  item_li.querySelector(".text-muted").textContent = `${item.price / 100} €`;
+  items_ul.appendChild(item_li);
+  item_li = items_ul.lastElementChild;
+  item_li.setAttribute('data-category',
+                       item.category.normalize("NFD").replace(/[\u0300-\u036f]/g, ""));
+  item_li.querySelector(".card-img-top").setAttribute('src', `/img/wedding-list/${item.image}`);
+}
+
+function _set_url_category(category) {
+  if (category == 'Toutes') {
+    url.searchParams.delete('category', category);
+    window.history.pushState("", "", location.pathname);
+  } else {
+    url.searchParams.set('category', category);
+    window.history.pushState("", "", `${location.pathname}?${url.searchParams}`);
   }
 }
 
