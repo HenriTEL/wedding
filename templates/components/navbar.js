cb4c6a918396b33028ea6e3be60ea2b98faeca12
Henri HANNETEL <henri.hannetel@gmail.com>
2020-05-25 13:04:50 +0200

    [index] added hang or married title

--- templates/components/navbar.js
+++ templates/components/navbar.js
@@ -1,12 +1,6 @@
 var url = new URL(location.href);
-
-function set_active_nav_item() {
-    for (let nav_link of document.querySelectorAll('.nav-link')) {
-        if (nav_link.pathname  == url.pathname) {
-            nav_link.parentElement.classList.add("active");
-            break;
-        }
+document.querySelectorAll('.nav-link').forEach(function (nav_link) {
+    if (nav_link.pathname == url.pathname) {
+        nav_link.parentElement.classList.add("active");
     }
-}
-
-set_active_nav_item();
+});
