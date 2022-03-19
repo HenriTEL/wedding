4c6b31bf3c0080d9c62994402dcc68a2c1c155fd
Henri HANNETEL <henri.hannetel@gmail.com>
2022-03-19 11:42:52 +0000

    Updated countdown

--- templates/components/countdown.js
+++ templates/components/countdown.js
@@ -1,28 +1,25 @@
 // Set the date we're counting down to
-var countDownDate = new Date("March 18, 2222 16:30:00").getTime();
-function update_countdown() {
+var date = new Date("March 18, 2222 16:30:00").getTime();
 
-    // Get today's date and time
-    var now = new Date().getTime();
-  
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-  
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+function update_countdown() {
+  var now = new Date().getTime();
+  var countdown = date - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(countdown / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((countdown % (1000 * 60)) / 1000);
   
     // If the count down is finished, write some text
     if (days == -1) {
       var text = "On se marie aujourd'hui !";
-    } else if (distance < 0) {
+    } else if (countdown < 0) {
       var text = "On s'est mariés !";
     } else {
       var text = `Mariés dans ${days} jour${days > 1 ? 's' : ''} ${hours} heure${hours > 1 ? 's' : ''} ${minutes} minute${minutes > 1 ? 's' : ''} ${seconds} seconde${seconds > 1 ? 's' : ''}`;
     }
-    document.getElementById("count-down").innerHTML = text;
+    document.getElementById("countdown").innerHTML = text;
   }
 // Initialize count down
 update_countdown();
